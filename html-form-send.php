b9e36448d55433203460ee05aa65f6eeb2367fdb
criollotasting <criollotasting@gmail.com>
2017-06-27 19:58:37 -0600

    Update html-form-send.php

--- html-form-send.php
+++ html-form-send.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
-    $email_to = "cindy@juniperbooks.com";
+    $email_to = "criollotasting@gmail.com";
      
     $email_subject = "Reservation";
      
